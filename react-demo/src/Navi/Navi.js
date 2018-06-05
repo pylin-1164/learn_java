e4b181fe06351e81b566108ba32db88b812899e1
puyulin <pyl1164@163.com>
2018-06-05 19:31:04 +0800

    左侧菜单动态展示

--- react-demo/src/Navi/Navi.js
+++ react-demo/src/Navi/Navi.js
@@ -9,20 +9,37 @@ import './Navi.css'
 const { Header, Content, Footer, Sider } = Layout;
 
 class SiderDemo extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+				date: '',
+				menus:[{"name":"组织管理","url":"http://localhost:3000/org","id":"1"},{"name":"用户管理","url":"http://localhost:3000/user","id":"2"}]
+		};
+	};
+	
+	
     state = {
         collapsed: false,
         mode: 'inline',
+        menus:[{"name":"组织管理","url":"http://localhost:3000/org","id":"1"},{"name":"用户管理","url":"http://localhost:3000/user","id":"2"}]
     };
+    
 
     toggle = () => {
         this.setState({
-            collapsed: !this.state.collapsed,
+            collapsed: !this.state.collapsed
         });
     };
     
-    handleClick = () =>{
-       alert(123);
-    };
+    handleClick(item,event){
+    	var menuItems = {"users":[{"name":"组织管理","url":"http://localhost:3000/org","id":"1"},{"name":"用户管理","url":"http://localhost:3000/user","id":"2"}],
+    			"org":[{"name":"电源管理","url":"http://localhost:3000/sys1","id":"3"},{"name":"IP管理","url":"http://locahost:3000/ip","id":"4"}],
+    			"sys":[{"name":"电源管理2","url":"http://localhost:3000/sys1","id":"5"},{"name":"IP管理2","url":"http://locahost:3000/ip","id":"6"}]
+    	};
+    	this.setState({
+    		menus: menuItems[item]
+        });
+    }
     
     render() {
     	
@@ -34,20 +51,18 @@ class SiderDemo extends Component {
                     collapsed={this.state.collapsed}
                 >
                     <div className="logo" />
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">
-                            <Icon type="user" />
-                            <span className="nav-text">nav 1</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="video-camera" />
-                            <span className="nav-text">nav 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Icon type="upload" />
-                            <span className="nav-text">nav 3</span>
-                        </Menu.Item>
-                    </Menu>
+                    <div id="leftMenus">
+	                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+	                        {
+	                        	this.state.menus.map(function(item){
+	                        		return <Menu.Item key={item.id}>
+		                        		<Icon type="user" />
+		                        		<span className="nav-text">{item.name}</span>
+		                        	</Menu.Item>
+		                        })
+	                        }
+		                </Menu>
+                    </div>
                 </Sider>
                 <Layout>
                     <Header style={{ background: '#000', padding: 0 ,height:68,minWidth:880}}>
@@ -63,15 +78,15 @@ class SiderDemo extends Component {
 			             </div>
 			             <div style={{float:'left',marginLeft:'25px'}}>
 				             <Menu theme="dark" mode="horizontal" style={{lineHeight:'68px'}} defaultSelectedKeys={['1']}>
-		                        <Menu.Item key="1" onClick={this.handleClick} >
+		                        <Menu.Item key="1" onClick={this.handleClick.bind(this,'users')} >
 		                            <Icon type="user"  />
 		                            <span className="nav-text">用户管理</span>
 		                        </Menu.Item>
-		                        <Menu.Item key="2" onClick={this.handleClick}>
+		                        <Menu.Item key="2" onClick={this.handleClick.bind(this,'org')}>
 		                            <Icon type="video-camera"/>
 		                            <span className="nav-text">角色管理</span>
 		                        </Menu.Item>
-		                        <Menu.Item key="3" onClick={this.handleClick}>
+		                        <Menu.Item key="3" onClick={this.handleClick.bind(this,'sys')}>
 		                            <Icon type="upload" />
 		                            <span className="nav-text">系统管理</span>
 		                        </Menu.Item>
@@ -86,7 +101,7 @@ class SiderDemo extends Component {
                             <Breadcrumb.Item>User</Breadcrumb.Item>
                             <Breadcrumb.Item>Bill</Breadcrumb.Item>
                         </Breadcrumb>
-                        <div style={{ padding: 24, background: '#fff', minHeight: 610 }}>
+                        <div style={{ padding: 24, background: '#fff', minHeight: 600 }}>
 
                         </div>
                     </Content>
